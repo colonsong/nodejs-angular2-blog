66b3bcea4ac2de1f3fa75fa3498f0325180a0cfe
colon <colon.song@gmail.com>
2017-02-22 15:25:57 +0800

    mysql connect

--- ServerApp/app.js
+++ ServerApp/app.js
@@ -1,5 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+require('dotenv').config();
+
+// DataBase 
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_BLOG
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log('connecting error');
+        return;
+    }
+    console.log('connecting success');
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+  if(err) console.log('err: ', err);
+  else console.log('Terminated done: ');
+});
+
 var app = express();
 
 var index = require('./routes/index');
@@ -8,6 +36,12 @@ var articles = require('./routes/articles');
 // 加在 router 前面，切記!!
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
+
+// db state
+app.use(function(req, res, next) {
+    req.con = con;
+    next();
+});
 
 // Set routers
 app.use('/', index);
