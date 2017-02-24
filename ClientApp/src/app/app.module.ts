14d259c0bb6254a9b9c9a2e9fea91bf345b7e010
colon <colon.song@gmail.com>
2017-02-24 16:57:36 +0800

    angular cli install & ng g c article

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { HttpModule } from '@angular/http';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
     HttpModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ArticleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
