1f4241e5899232fba4977f50218538d6e5086b0b
colon <colon.song@gmail.com>
2017-03-18 03:04:51 +0800

    add mysql conn error handle

--- ServerApp/db/mysql.js
+++ ServerApp/db/mysql.js
@@ -2,19 +2,20 @@ require('dotenv').config();
 
 // DataBase 
 var mysql = require("mysql");
-
-var con = mysql.createConnection({
+var db_option = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME_BLOG,
     debug: true,
+};
+var con = mysql.createConnection(db_option);
+con.on('error', function(err){
+
+    console.log(err);
 });
-
-
 // Get records from a city
 exports.getArticles = function(page, callback) {
-    
     var perPage = 10;
     var start = (page-1) * perPage;
     const end = start + perPage;
@@ -23,12 +24,12 @@ exports.getArticles = function(page, callback) {
     "where hide = 0 order by `blogContents_id` desc limit ?,?";
 
     con.query(sql,[start,end], function(err, rows, fields){
-        if(err) { console.log(err); }
+        if(err) throw err;
         console.log('Data received from Db:\n');
         console.log(rows);
         var sql = "SELECT count(*) c FROM `blogContents` where hide = 0  ";
         con.query(sql, function(err, count, fields){
-            if(err) { console.log(err); }
+            if(err) throw err;
             console.log(count);
             callback(err, {"count":count[0].c, "data":rows});
         });
@@ -39,10 +40,11 @@ exports.getArticles = function(page, callback) {
 
 // Get records from a city
 exports.getArticleById = function(id, callback) {
+    
     var sql = "SELECT title,blogContents_id id, classify, datetime, contents FROM `blogContents` where blogContents_id = ? AND hide = 0 ";
     
     con.query(sql, [id], function(err, rows, fields){
-        if(err) { console.log(err); }
+        if(err) throw err;
         
         console.log('Data received from Db:\n');
         console.log(rows);
@@ -53,9 +55,10 @@ exports.getArticleById = function(id, callback) {
 
 // Get records from a city
 exports.searchArticles = function(term, callback) {
+   
     var sql = "SELECT title,blogContents_id id, classify, datetime, contents FROM `blogContents` where title like ? OR contents like ? limit 20";
     con.query(sql, ['%' + term + '%','%' + term + '%'], function(err, rows, fields){
-        if(err) { console.log(err); }
+        if(err) throw err;
         console.log('Data received from Db:\n');
         console.log(rows);
         callback(err, rows);
@@ -65,7 +68,7 @@ exports.searchArticles = function(term, callback) {
 exports.getCategorys = function(term, callback) {
     var sql = "SELECT `classify` , COUNT(  `classify` ) c FROM  `blogContents` where `hide` = 0 GROUP BY  `classify` ORDER BY  `c` DESC ";
     con.query(sql, function(err, rows, fields){
-        if(err) { console.log(err); }
+        if(err) throw err;
         
         console.log('Data received from Db:\n');
         console.log(rows);
@@ -75,10 +78,11 @@ exports.getCategorys = function(term, callback) {
 
 // 文章目錄
 exports.getArticlesByClassify = function(classify, callback) {
+    
     var sql = "SELECT *,DATE_FORMAT(update_time, '%W, %M, %d, %Y, %H:%i:%s') d " +  
     "FROM `blogContents` WHERE classify=? ORDER BY  `update_time` DESC limit 10";
     con.query(sql, [classify], function(err, rows, fields){
-        if(err) { console.log(err); }
+        if(err) throw err;
         console.log('Data received from Db:\n');
         console.log(rows);
         callback(err, rows);
